04149cfd5e6fdf3f9b0df8581c0f3b54da7cfbd0
Biró Dániel <biro.daniel@nexogen.hu>
2019-12-02 11:48:45 +0100

    Day01 solver

---
+++ AdventOfCode.2019.Day01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode._2019.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputData = File.ReadAllLines("input.txt");
+            int[] inputMasses = inputData.Select(i => int.Parse(i.Trim())).ToArray();
+            var masses = ImmutableList.Create(inputMasses);
+
+            int sumOfFuelRequirements = 0;
+            foreach (var mass in masses)
+            {
+                int fuelRequirement = mass / 3 - 2;
+                sumOfFuelRequirements += fuelRequirement;
+            }
+
+            Console.WriteLine(sumOfFuelRequirements);
+        }
+    }
+}
