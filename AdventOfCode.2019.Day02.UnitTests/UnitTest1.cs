c85163b143ed568ffdbfb21a11e40059a1900307
Biró Dániel <biro.daniel@nexogen.hu>
2019-12-09 11:10:47 +0100

    Solution restructuing, empty unit test project for day02

---
+++ AdventOfCode.2019.Day02.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode._2019.Day02.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
