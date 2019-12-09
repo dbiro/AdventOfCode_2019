2c096b7ca2ca6784709ed0cba0e6ff1d6f01213c
dbiro <birodani897@gmail.com>
2019-12-09 23:06:05 +0100

    Day03 skeleton

---
+++ AdventOfCode.2019.Day03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode._2019.Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
