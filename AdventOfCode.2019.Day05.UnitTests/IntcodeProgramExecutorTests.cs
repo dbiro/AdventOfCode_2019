e7c1b524773fc1e7563760decc573f4006c3400b
dbiro <birodani897@gmail.com>
2019-12-11 21:49:33 +0100

    Day05 - one more test

--- AdventOfCode.2019.Day05.UnitTests/IntcodeProgramExecutorTests.cs
+++ AdventOfCode.2019.Day05.UnitTests/IntcodeProgramExecutorTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
 
 namespace AdventOfCode._2019.Day05.UnitTests
 {
@@ -11,11 +13,11 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 1, 0, 0, 0, 99 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 2);
-            Assert.AreEqual(output[1], 0);
-            Assert.AreEqual(output[2], 0);
-            Assert.AreEqual(output[3], 0);
-            Assert.AreEqual(output[4], 99);
+            Assert.AreEqual(2, output[0]);
+            Assert.AreEqual(0, output[1]);
+            Assert.AreEqual(0, output[2]);
+            Assert.AreEqual(0, output[3]);
+            Assert.AreEqual(99, output[4]);
         }
 
         [TestMethod]
@@ -24,11 +26,11 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 2, 3, 0, 3, 99 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 2);
-            Assert.AreEqual(output[1], 3);
-            Assert.AreEqual(output[2], 0);
-            Assert.AreEqual(output[3], 6);
-            Assert.AreEqual(output[4], 99);
+            Assert.AreEqual(2, output[0]);
+            Assert.AreEqual(3, output[1]);
+            Assert.AreEqual(0, output[2]);
+            Assert.AreEqual(6, output[3]);
+            Assert.AreEqual(99, output[4]);
         }
 
         [TestMethod]
@@ -37,12 +39,12 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 2, 4, 4, 5, 99, 0 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 2);
-            Assert.AreEqual(output[1], 4);
-            Assert.AreEqual(output[2], 4);
-            Assert.AreEqual(output[3], 5);
-            Assert.AreEqual(output[4], 99);
-            Assert.AreEqual(output[5], 9801);
+            Assert.AreEqual(2, output[0]);
+            Assert.AreEqual(4, output[1]);
+            Assert.AreEqual(4, output[2]);
+            Assert.AreEqual(5, output[3]);
+            Assert.AreEqual(99, output[4]);
+            Assert.AreEqual(9801, output[5]);
         }
 
         [TestMethod]
@@ -51,15 +53,15 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 30);
-            Assert.AreEqual(output[1], 1);
-            Assert.AreEqual(output[2], 1);
-            Assert.AreEqual(output[3], 4);
-            Assert.AreEqual(output[4], 2);
-            Assert.AreEqual(output[5], 5);
-            Assert.AreEqual(output[6], 6);
-            Assert.AreEqual(output[7], 0);
-            Assert.AreEqual(output[8], 99);
+            Assert.AreEqual(30, output[0]);
+            Assert.AreEqual(1, output[1]);
+            Assert.AreEqual(1, output[2]);
+            Assert.AreEqual(4, output[3]);
+            Assert.AreEqual(2, output[4]);
+            Assert.AreEqual(5, output[5]);
+            Assert.AreEqual(6, output[6]);
+            Assert.AreEqual(0, output[7]);
+            Assert.AreEqual(99, output[8]);
         }
 
         [TestMethod]
@@ -68,11 +70,11 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 1002, 4, 3, 4, 33 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 1002);
-            Assert.AreEqual(output[1], 4);
-            Assert.AreEqual(output[2], 3);
-            Assert.AreEqual(output[3], 4);
-            Assert.AreEqual(output[4], 99);            
+            Assert.AreEqual(1002, output[0]);
+            Assert.AreEqual(4, output[1]);
+            Assert.AreEqual(3, output[2]);
+            Assert.AreEqual(4, output[3]);
+            Assert.AreEqual(99, output[4]);
         }
 
         [TestMethod]
@@ -81,11 +83,31 @@ namespace AdventOfCode._2019.Day05.UnitTests
             int[] input = new int[] { 1101, 100, -1, 4, 0 };
             int[] output = IntcodeProgramExecutor.Execute(input);
 
-            Assert.AreEqual(output[0], 1101);
-            Assert.AreEqual(output[1], 100);
-            Assert.AreEqual(output[2], -1);
-            Assert.AreEqual(output[3], 4);
-            Assert.AreEqual(output[4], 99);
+            Assert.AreEqual(1101, output[0]);
+            Assert.AreEqual(100, output[1]);
+            Assert.AreEqual(-1, output[2]);
+            Assert.AreEqual(4, output[3]);
+            Assert.AreEqual(99, output[4]);
+        }
+
+        [TestMethod]
+        public void TestCase_07()
+        {
+            int[] input = new int[] { 3, 0, 4, 0, 99 };
+            var stringBuilder = new StringBuilder();
+
+            IntcodeProgramExecutor.InputReader = new StringReader("11");
+            IntcodeProgramExecutor.OutputWriter = new StringWriter(stringBuilder);
+
+            int[] output = IntcodeProgramExecutor.Execute(input);
+
+            Assert.AreEqual(11, output[0]);
+            Assert.AreEqual(0, output[1]);
+            Assert.AreEqual(4, output[2]);
+            Assert.AreEqual(0, output[3]);
+            Assert.AreEqual(99, output[4]);
+
+            Assert.IsTrue(stringBuilder.ToString().Contains("Output value: 11"));
         }
     }
 }
