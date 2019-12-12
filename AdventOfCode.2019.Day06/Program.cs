bf287203de47a36b17cc2b133e95b62c728114bf
Biró Dániel <biro.daniel@nexogen.hu>
2019-12-12 17:56:34 +0100

    Day 06 - save state

--- AdventOfCode.2019.Day06/Program.cs
+++ AdventOfCode.2019.Day06/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AdventOfCode._2019.Day06
 {
+    class Space
+    {
+        public SpaceObject COM { get; }
+
+
+    }
+
+    class SpaceObject
+    {
+        public int Value { get; private set; }
+
+        IEnumerable<SpaceObject> Children { get; }
+    }
+
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] orbits = File.ReadAllLines("intput.txt");
+            var orbitMap = new Dictionary<string, IEnumerable<string>>();
+            foreach (var o in orbits)
+            {
+                
+            }
         }
     }
 }
