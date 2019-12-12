1a653a2a6503fb1f24d845fe20f0c3b7e0b16547
dbiro <birodani897@gmail.com>
2019-12-12 22:33:05 +0100

    Day06 - part1 solution

--- AdventOfCode.2019.Day06/Program.cs
+++ AdventOfCode.2019.Day06/Program.cs
@@ -1,36 +1,68 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
 namespace AdventOfCode._2019.Day06
 {
-    class Space
-    {
-        public SpaceObject COM { get; }
-
-
-    }
-
-    class SpaceObject
-    {
-        public int Value { get; private set; }
-
-        IEnumerable<SpaceObject> Children { get; }
-    }
-
     class Program
     {
-        
-
         static void Main(string[] args)
         {
-            string[] orbits = File.ReadAllLines("intput.txt");
-            var orbitMap = new Dictionary<string, IEnumerable<string>>();
-            foreach (var o in orbits)
+            string[] orbits = File.ReadAllLines("input.txt");
+
+            //string[] orbits = new string[]
+            //{
+            //    "COM)B",
+            //    "B)C",
+            //    "C)D",
+            //    "D)E",
+            //    "E)F",
+            //    "B)G",
+            //    "G)H",
+            //    "D)I",
+            //    "E)J",
+            //    "J)K",
+            //    "K)L"
+            //};
+
+            var orbitMap = new Dictionary<string, ICollection<string>>();
+            foreach (var orbit in orbits)
             {
-                
+                string[] spaceObjects = orbit.Split(")");
+                if (!orbitMap.ContainsKey(spaceObjects[0]))
+                {
+                    orbitMap[spaceObjects[0]] = new List<string>();
+                }
+                orbitMap[spaceObjects[0]].Add(spaceObjects[1]);
             }
+
+            int orbitCount = TraverseOrbitMap(orbitMap);
+
+            Console.WriteLine(orbitCount);
+        }
+
+        static int TraverseOrbitMap(Dictionary<string, ICollection<string>> orbitMap)
+        {
+            int orbitCount = 0;
+            TraverseSpaceObject("COM", orbitMap, 0, ref orbitCount);
+            return orbitCount;
+        }
+
+        static void TraverseSpaceObject(string spaceObject, Dictionary<string, ICollection<string>> orbitMap, int depth, ref int orbitCount)
+        {            
+            if (orbitMap.ContainsKey(spaceObject))
+            {
+                orbitCount += depth;
+
+                foreach (var child in orbitMap[spaceObject])
+                {
+                    TraverseSpaceObject(child, orbitMap, depth + 1, ref orbitCount);
+                }
+            }
+            else
+            {                
+                orbitCount += depth;
+            }            
         }
     }
 }
