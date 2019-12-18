5b2f180bcbff43b1b2cb27f4b35d7357072ebc9a
Biró Dániel <biro.daniel@nexogen.hu>
2019-12-18 18:10:11 +0100

    Day13 - part two visualization

--- AdventOfCode.2019.Day13/Program.cs
+++ AdventOfCode.2019.Day13/Program.cs
@@ -8,14 +8,65 @@ namespace AdventOfCode._2019.Day13
     {
         enum TileType { Empty = 0, Wall = 1, Block = 2, HorizontalPaddle = 3, Ball = 4 }
 
+        static char PrintTileType(TileType tileType)
+        {
+            switch (tileType)
+            {
+                case TileType.Empty:
+                    return ' ';
+                case TileType.Wall:
+                    return 'W';
+                case TileType.Block:
+                    return 'B';
+                case TileType.HorizontalPaddle:
+                    return 'P';
+                case TileType.Ball:
+                    return 'O';
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        static void PrintTiles(Dictionary<long, Dictionary<long, TileType>> tiles)
+        {            
+            foreach (var px in tiles)
+            {
+                foreach (var py in tiles[px.Key])
+                {
+                    Console.SetCursorPosition((int)px.Key, (int)py.Key);
+                    Console.Write(PrintTileType(py.Value));
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             long[] inputProgram = InputReader.Read();
-
-            Func<long?> inputReader = () => throw new NotSupportedException();
+            inputProgram[0] = 2;
+                        
+            long userScore = 0;
 
             var outputs = new Queue<long>(3);
             var tiles = new Dictionary<long, Dictionary<long, TileType>>();
+
+            Func<long?> inputReader = () =>
+            {
+                PrintTiles(tiles);
+
+                long ret = 0;
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    ret = -1;
+                }
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    ret = 1;
+                }
+                
+                return ret;
+            };            
 
             Action<long> outputWriter = output =>
             {
@@ -25,26 +76,36 @@ namespace AdventOfCode._2019.Day13
                 {
                     long posX = outputs.Dequeue();
                     long posY = outputs.Dequeue();
-                    TileType tileType = (TileType)outputs.Dequeue();
 
-                    if (!tiles.ContainsKey(posX))
+                    if (posX == -1 && posY == 0)
                     {
-                        tiles[posX] = new Dictionary<long, TileType>();
+                        userScore = outputs.Dequeue();
+                        Console.SetCursorPosition(0, (int)tiles.Values.SelectMany(py => py.Keys).Max() + 1);
+                        Console.Write($"Score: {userScore}");
                     }
+                    else
+                    {
+                        TileType tileType = (TileType)outputs.Dequeue();
 
-                    tiles[posX][posY] = tileType;
+                        if (!tiles.ContainsKey(posX))
+                        {
+                            tiles[posX] = new Dictionary<long, TileType>();
+                        }
+
+                        tiles[posX][posY] = tileType;
+                    }                    
                 }
             };
 
             var gameProgram = new IntcodeProgram(inputProgram, inputReader, outputWriter);
-            gameProgram.Execute();
+            gameProgram.Execute();                        
 
             if (!gameProgram.Halted)
             {
                 throw new InvalidOperationException();
             }
 
-            Console.WriteLine(tiles.Values.SelectMany(py => py.Values).Count(t => t == TileType.Block));
+            //Console.WriteLine(tiles.Values.SelectMany(py => py.Values).Count(t => t == TileType.Block));
         }
     }
 }
