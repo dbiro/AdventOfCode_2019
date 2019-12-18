56e07d24318514e5bb85cb172790ce5fec2ad0e1
Biró Dániel <biro.daniel@nexogen.hu>
2019-12-18 11:01:04 +0100

    Day12 - part one solution

---
+++ AdventOfCode.2019.Day12/Moon.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AdventOfCode._2019.Day12
+{
+    class Moon
+    {
+        private (int X, int Y, int Z) position;
+        private (int X, int Y, int Z) velocity;
+
+        public int KineticEnergy
+            => Math.Abs(velocity.X) + Math.Abs(velocity.Y) + Math.Abs(velocity.Z);
+
+        public int PotentialEnergy
+            => Math.Abs(position.X) + Math.Abs(position.Y) + Math.Abs(position.Z);
+
+        public int TotalEnergy
+            => KineticEnergy * PotentialEnergy;
+
+        public Moon(int posX, int posY, int posZ)
+        {
+            this.position = (posX, posY, posZ);
+            velocity = (0, 0, 0);
+        }
+
+        public void ApplyVelocity()
+        {
+            position.X += velocity.X;
+            position.Y += velocity.Y;
+            position.Z += velocity.Z;
+        }
+
+        public void ApplyGravity(Moon other)
+        {            
+            if (position.X > other.position.X)
+            {
+                velocity.X--;
+            }
+            else if (position.X < other.position.X)
+            {
+                velocity.X++;
+            }
+
+            if (position.Y > other.position.Y)
+            {
+                velocity.Y--;
+            }
+            else if (position.Y < other.position.Y)
+            {
+                velocity.Y++;
+            }
+
+            if (position.Z > other.position.Z)
+            {
+                velocity.Z--;
+            }
+            else if (position.Z < other.position.Z)
+            {
+                velocity.Z++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"pos=<x={position.X}, y={position.Y}, z={position.Z}>, vel=<x={velocity.X}, y={velocity.Y}, z={velocity.Z}>";
+        }
+    }
+}
---
+++ AdventOfCode.2019.Day12/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2019.Day12
+{
+    class Program
+    {
+        static void PrintMoons(IEnumerable<Moon> moons)
+        {
+            foreach (var moon in moons)
+            {
+                Console.WriteLine(moon);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            /*  INPUT */
+            List<Moon> moons = new List<Moon>()
+            {
+                new Moon(14, 15, -2),
+                new Moon(17, -3, 4),
+                new Moon(6, 12, -13),
+                new Moon(-2, 10, -8)
+            };
+
+            /* SAMPLE1 */
+            //List<Moon> moons = new List<Moon>()
+            //{
+            //    new Moon(-1, 0, 2),
+            //    new Moon(2, -10, -7),
+            //    new Moon(4, -8, 8),
+            //    new Moon(3, 5, -1)
+            //};
+
+            /*  SAMPLE2 */
+            //List<Moon> moons = new List<Moon>()
+            //{
+            //    new Moon(-8, -10, 0),
+            //    new Moon(5, 5, 10),
+            //    new Moon(2, -7, 3),
+            //    new Moon(9, -8, -3)
+            //};
+
+            PrintMoons(moons);
+
+            const int steps = 1000;
+
+            for (int i = 0; i < steps; i++)
+            {
+                foreach (var moon in moons)
+                {
+                    foreach (var otherMoon in moons)
+                    {
+                        moon.ApplyGravity(otherMoon);
+                    }
+
+                }
+
+                foreach (var moon in moons)
+                {
+                    moon.ApplyVelocity();
+                }
+
+                //PrintMoons(moons);
+            }
+
+            Console.WriteLine(moons.Sum(m => m.TotalEnergy));            
+        }
+    }
+}
